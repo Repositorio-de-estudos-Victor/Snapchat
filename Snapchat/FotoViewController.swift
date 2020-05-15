ae3f55e358a0ec3df0f3b8bdcb6e25667d46dd90
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-05-15 19:16:38 -0300

    poc

--- Snapchat/FotoViewController.swift
+++ Snapchat/FotoViewController.swift
@@ -8,23 +8,12 @@
 
 import UIKit
 
-class FotoViewController: UIViewController {
+class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
